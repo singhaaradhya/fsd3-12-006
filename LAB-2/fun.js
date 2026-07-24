74fd7d19af58983e9259ef4960a7881ea054356a
singhaaradhya <reachaaradhya2@gmail.com>
2026-07-24 15:40:43 +0530

    lab-2(fun)

---
+++ LAB-2/fun.js
@@ -0,0 +1,19 @@
+const f1 = () =>{
+    console.log("F1");
+
+};
+const f2 = () =>{
+    console.log("F2");
+};
+const f3 = () =>{
+    console.log("F3");
+};
+
+function main(){
+    console.log("main");
+    f1();
+    f2();
+    f3();
+    console.log("end");
+}
+main();
