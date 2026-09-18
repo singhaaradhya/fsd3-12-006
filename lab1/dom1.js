a40df7b750f9881f3111b11183074a8f1061c8da
singhaaradhya <reachaaradhya2@gmail.com>
2026-09-18 14:57:02 +0530

    update dom1

--- lab1/dom1.js
+++ lab1/dom1.js
@@ -1,11 +1,11 @@
 // DOM - document object model
-import{EventEmitter} from "events";
+import { EventEmitter } from "events";
 
 const button = new EventEmitter();
 
 button.on("click", () => {
-    console.log("Button clicked");
+  console.log("Button clicked");
+  console.log("hello buton click");
 });
 
 button.emit("click");
- 
